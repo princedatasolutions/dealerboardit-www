bedaa846d538a40fa237b56e03793d78c9ae4275
Stephen Prince <swprince1303@gmail.com>
2025-11-06 08:54:26 -0600

    update pricing

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
                 onClick={() => openModal("monthly")}
                 aria-label="Monthly plan"
               >
-                <span className="fs-accent fs-amount">$250</span>
+                <span className="fs-accent fs-amount">$300</span>
                 <span className="fs-note">
                   Per Month
                   <br />
@@ -78,7 +78,7 @@ export default function Home() {
                 onClick={() => openModal("multi_store")}
                 aria-label="Monthly discount with multi-stores"
               >
-                <span className="fs-accent fs-amount">$225</span>
+                <span className="fs-accent fs-amount">$270</span>
                 <span className="fs-note">
                   Per Month
                   <br />
@@ -95,7 +95,7 @@ export default function Home() {
                 onClick={() => openModal("yearly")}
                 aria-label="Yearly plan - 2 months free"
               >
-                <span className="fs-accent fs-amount">$200</span>
+                <span className="fs-accent fs-amount">$240</span>
                 <span className="fs-note">
                   Per Month
                   <br />
